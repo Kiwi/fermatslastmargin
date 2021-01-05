719b44ecface9b6a3409db6fcdf3e28269558cb3
Robert Djubek <envy1988@gmail.com>
2021-01-05 22:17:08 +0000

    haskell-language-server

--- nix/default.nix
+++ nix/default.nix
@@ -1,22 +1,24 @@
 { }:
 with rec {
   sources = import ./sources.nix;
-  pkgs_ = import sources.nixpkgs {};
+  pkgs_ = import sources.nixpkgs { };
   pkgs = import sources.nixpkgs {
     overlays = [
-      (self: super: super.lib.recursiveUpdate super {
-        haskell = super.lib.recursiveUpdate super.haskell {
-          packages = super.lib.recursiveUpdate super.haskell.packages {
-            ghc881 = super.haskell.packages.ghc881.override (old: {
-              overrides = super.lib.composeExtensions
-                (old.overrides or (_: _: {}))
-                (hself: hsuper: {
-                  fermatslastmargin = hself.callCabal2nix "fermatslastmargin" (super.lib.cleanSource ../.) {};
-                });
-            });
+      (self: super:
+        super.lib.recursiveUpdate super {
+          haskell = super.lib.recursiveUpdate super.haskell {
+            packages = super.lib.recursiveUpdate super.haskell.packages {
+              ghc8102 = super.haskell.packages.ghc8102.override (old: {
+                overrides =
+                  super.lib.composeExtensions (old.overrides or (_: _: { }))
+                  (hself: hsuper: {
+                    fermatslastmargin = hself.callCabal2nix "fermatslastmargin"
+                      (super.lib.cleanSource ../.) { };
+                  });
+              });
+            };
           };
-        };
-      })
+        })
     ];
   };
 
@@ -25,5 +27,5 @@ with rec {
 rec {
   inherit pkgs;
   inherit sources;
-  hsPkgs = pkgs.haskell.packages.ghc881;
+  hsPkgs = pkgs.haskell.packages.ghc8102;
 }
--- shell.nix
+++ shell.nix
@@ -1,19 +1,20 @@
 with (import ./default.nix);
 
 hsPkgs.shellFor {
-  packages = ps: with ps; [
-    fermatslastmargin
-  ];
+  packages = ps: with ps; [ fermatslastmargin ];
 
   withHoogle = false;
 
-  buildInputs = with hsPkgs; with pkgs; [
-    hlint
-    stylish-haskell
-    ghcid
-    cabal-install
-    poppler_utils
-  ];
+  buildInputs = with hsPkgs;
+    with pkgs; [
+      cabal-install
+      ghcid
+      haskell-language-server
+      hlint
+      poppler_utils
+      stylish-haskell
+      git
+    ];
 
   exactDeps = true;
 }
